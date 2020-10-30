26eb546e0acc91265093c6af36ad6294bac9614a
bvfr <67278922+bvfr@users.noreply.github.com>
2020-10-30 16:59:00 -0400

    Functionality for turning arrows around

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-
-
-/*function openItem() {
-  document.getElementById('add-on').style.display = 'block';
-}*/
-
-
 function openItem(target) {
   document.getElementById("add-on").style.display = "block";
   target.style.display = "none";
@@ -13,4 +6,14 @@ function openItem(target) {
 function hideItem(target) {
   document.getElementById("add-on").style.display = "none";
   document.getElementById("plan-add-on-button").style.display = "flex";
+}
+
+function openHideMobile(target, arrow) {
+  if (target.style.display === "none") {
+    arrow.style.transform = "rotateX(180deg)";
+    target.style.display = "block";
+  } else {
+    arrow.style.transform = "rotateX(0deg)";
+    target.style.display = "none";
+  }
 }
